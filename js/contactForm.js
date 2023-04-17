92dccb57f835fb5695b5622c0a06da74e3233f9d
matiscava <matiscava@hotmail.com>
2023-04-17 13:18:03 -0300

    Cambie el mail del formulario de contacto

--- js/contactForm.js
+++ js/contactForm.js
@@ -6,7 +6,7 @@
   $form.addEventListener( 'submit', (e) => {
     e.preventDefault();
     $loader.classList.remove('none');
-    fetch('https://formsubmit.co/ajax/manuelcastellano1207@gmail.com', {
+    fetch('https://formsubmit.co/ajax/info@tnmtutors.com', {
       method: 'POST',
       body: new FormData(e.target)
     })
